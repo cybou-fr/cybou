635a92ea212c63ffe370b4817c5cd873578e2726
Stanislav Saveliev <ioustamora@gmail.com>
2026-09-23 00:02:32 +0200

    introduce isolated CYBOU-DEV network parameters

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -569,18 +569,6 @@ int GuiMain(int argc, char* argv[])
         return EXIT_SUCCESS;
     }
 
-#ifdef CYBOU_NETWORK_QUARANTINED
-    // Fail closed until dedicated CYBOU-DEV chain parameters (genesis, network
-    // magic, ports, and discovery policy) replace every inherited Bitcoin
-    // network. Help and version output remain available for build validation.
-    const QString quarantine_error = QObject::tr(
-        "CYBOU network quarantine is active. Node startup is disabled until "
-        "dedicated CYBOU-DEV chain parameters are installed.");
-    InitError(Untranslated(quarantine_error.toStdString()));
-    QMessageBox::critical(nullptr, CLIENT_NAME, quarantine_error);
-    return EXIT_FAILURE;
-#endif
-
     // Install global event filter that makes sure that long tooltips can be word-wrapped
     app.installEventFilter(new GUIUtil::ToolTipToRichTextFilter(TOOLTIP_WRAP_THRESHOLD, &app));
 
@@ -608,6 +596,18 @@ int GuiMain(int argc, char* argv[])
             // message in the case of a read error if the user decided to abort.
             QMessageBox::critical(nullptr, CLIENT_NAME, QObject::tr("Error: %1").arg(QString::fromStdString(error->message.translated)));
         }
+        return EXIT_FAILURE;
+    }
+
+    // The consumer application exposes only the isolated CYBOU-DEV network.
+    // Inherited Bitcoin test networks remain compiled for migration tests but
+    // must never be selectable by the public desktop executable.
+    if (Params().GetChainType() != ChainType::MAIN) {
+        const QString network_error = QObject::tr(
+            "This CYBOU build supports CYBOU-DEV only. Bitcoin testnet, "
+            "testnet4, signet, and regtest are disabled.");
+        InitError(Untranslated(network_error.toStdString()));
+        QMessageBox::critical(nullptr, CLIENT_NAME, network_error);
         return EXIT_FAILURE;
     }
 #ifdef ENABLE_WALLET
---
+++ src/test/cybou_chainparams_tests.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2026 The CYBOU developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or https://opensource.org/license/mit/.
+
+#include <chainparamsbase.h>
+#include <kernel/chainparams.h>
+#include <uint256.h>
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(cybou_chainparams_tests)
+
+BOOST_AUTO_TEST_CASE(dev_network_is_isolated)
+{
+    const auto params = CChainParams::Main();
+
+    BOOST_CHECK_EQUAL(params->GetDefaultPort(), 29444);
+    BOOST_CHECK_EQUAL(CreateBaseChainParams(ChainType::MAIN)->RPCPort(), 29443);
+    BOOST_CHECK(params->DNSSeeds().empty());
+    BOOST_CHECK(params->FixedSeeds().empty());
+    BOOST_CHECK_EQUAL(params->Bech32HRP(), "cyb");
+    BOOST_CHECK(params->GetAvailableSnapshotHeights().empty());
+
+    const MessageStartChars expected_magic{0xac, 0x9e, 0x78, 0x64};
+    BOOST_CHECK(params->MessageStart() == expected_magic);
+    BOOST_CHECK_EQUAL(
+        params->GetConsensus().hashGenesisBlock.GetHex(),
+        "189801dbef446c3d45b89b5a0e0a30e8b50da8447c4cee454dc8e93f96515327");
+    BOOST_CHECK_EQUAL(
+        params->GenesisBlock().hashMerkleRoot.GetHex(),
+        "a1cc066afc59f4a9b219a94f5e185772260ac1af43a4508bfc06acc61da91cb2");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
